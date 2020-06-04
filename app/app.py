c3703ca9c152ae757d8d405c47a44bb8864a2619
xradev <xradev@abv.bg>
2020-06-04 16:47:30 +0300

    Test GUI commit

--- app/app.py
+++ app/app.py
@@ -1,7 +1,3 @@
-#### IMPORTANT ####
-# Yes, yes i know Redis should not be used to store persistent data.
-# This program is made for the sole purpose of practicing Redis w/ python.
-
 import flask
 import app.cron_jobs as cron
 
